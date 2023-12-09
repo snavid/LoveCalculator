2f9dfe27e16411699967c968483de91b636b7249
snavid <snavidux@outlook.com>
2023-12-09 21:17:12 +0300

    added description

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ from setuptools import setup, find_packages
 setup(
     name="LoveCalculator",
     version="0.1.0",
+    description='An enchanting Python module designed to ignite the flames of affection! Dive into the realm of romance as PyLoveCalc seamlessly computes couple names and unveils the mystical percentage of love that binds them together.',
     packages=find_packages(),
     install_requires=[],
     entry_points={},
