0ee849f5eb5ca6dc7b3438a9830617a2dee9fa73
YUNUS <105539783+snavid@users.noreply.github.com>
2023-12-09 21:00:37 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name="LoveCalculator",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[],
+    entry_points={},
+)
