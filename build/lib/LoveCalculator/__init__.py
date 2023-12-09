a192f61f7b266a13a5d4cad9a04c1e16b18aeb28
snavid <snavidux@outlook.com>
2023-12-09 18:09:45 +0300

    save

--- build/lib/LoveCalculator/__init__.py
+++
@@ -1,9 +0,0 @@
-from .Calculator import LoveCalc, ChangeToInteger, WhichIsGreater
-
-def BestLoveCalc(input1, input2):
-    value1 = LoveCalc(input1, input2)
-    value2 = LoveCalc(input2, input1)
-    return WhichIsGreater(ChangeToInteger(value1), ChangeToInteger(value2))
-
-def NormalLoveCalc(input1, input2):
-    return LoveCalc(input1, input2)
