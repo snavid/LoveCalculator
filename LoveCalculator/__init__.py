15547c5f3747a965d15fe827efb3d0766b8a2e50
snavid <snavidux@outlook.com>
2023-12-09 22:14:56 +0300

    added version name

--- LoveCalculator/__init__.py
+++ LoveCalculator/__init__.py
@@ -1,5 +1,6 @@
 from .Calculator import LoveCalc, ChangeToInteger, WhichIsGreater
 
+__version__ = "1.0.1"
 def BestLoveCalc(input1, input2):
     value1 = LoveCalc(input1, input2)
     value2 = LoveCalc(input2, input1)
